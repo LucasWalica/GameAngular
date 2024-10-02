a136bc6d972920638e44e9fa9db3e63208530e45
Lucas Walica <Projectwali272@gmail.com>
2024-10-02 18:59:31 +0200

    minor changes

--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -13,7 +13,7 @@ export class MapService {
       "In this zone of the city the upper class was living until the invasion, there are many security systems and maybe some treasures that can be sold on the black market.", 
       6, 10, 40, 20, 30),
       new Map("Parking", "../../assets/maps/city3.jpg", 
-      "This is the supermarket parking, the other ways to enter are blocked, if you need food and medication is the best place to search for them, but before you enter, you will hace some troubles with the infected ones.",
+      "This is the supermarket parking, the other ways to enter are blocked, if you need food and medication is the best place to search for them, but before you enter, you will have some troubles with the infected ones.",
       8, 20, 30, 40, 10),
       new Map("Industrial Zone", "../../assets/maps/city4.jpg", 
       "The old industrial zone is still active because the remaining survivors need electricity, if the resistance here falls, all of us will get infected, come here to help the alliance.", 
